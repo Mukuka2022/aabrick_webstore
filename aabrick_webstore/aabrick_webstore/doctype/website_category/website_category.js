108d9fbb53ce01d800153a915d2edaf41c8115ea
dawoodjee <adam.dawoodjee@gmail.com>
2026-09-06 10:13:01 +0000

    first commit

---
+++ aabrick_webstore/aabrick_webstore/doctype/website_category/website_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Adam & Mukuka and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Website Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
